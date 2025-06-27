92401d59aa7ddb9eddca8b411c37cb2f855438a9
Rafaela Mirabile <rafaelamirabile@Rafaelas-Air.Home>
2025-06-27 13:29:34 +0100

    Home.tsx created: Suspense realocated to page.tsx

---
+++ app/home.tsx
@@ -0,0 +1,43 @@
+'use client';
+import Projects from "@/components/projects/projects";
+import GetInTouch from '@/components/shared/GetInTouch';
+import PageMetaHeads from "@/components/shared/PageMetaHeads";
+import Tools from "@/components/tools/tools";
+import './globals.css';
+import Hero from "@/components/hero/hero";
+import { useEffect, Suspense } from "react";
+import { useSearchParams } from "next/navigation";
+import Experience from "@/components/experience/experience";
+
+
+export default function Home() {
+  const searchParams = useSearchParams();
+
+useEffect(() => {
+  const scrollTo = searchParams.get('scrollTo');
+  if (scrollTo) {
+    const section = document.getElementById(scrollTo);
+    if (section) {
+      setTimeout(() => {
+        section.scrollIntoView({ behavior: 'smooth' });
+        window.history.replaceState({}, '', window.location.pathname);
+      }, 100);
+    }
+  }
+}, [searchParams]);
+
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+    <>
+      <PageMetaHeads title="Home | My Portfolio" />
+      <div>
+        <Hero/>
+        <Projects />
+        <Tools />
+        <Experience />
+        <GetInTouch />
+      </div>
+    </>
+    </Suspense>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,10 @@
-'use client';
-import Projects from "@/components/projects/projects";
-import GetInTouch from '@/components/shared/GetInTouch';
-import PageMetaHeads from "@/components/shared/PageMetaHeads";
-import Tools from "@/components/tools/tools";
-import './globals.css';
-import Hero from "@/components/hero/hero";
-import { useEffect, Suspense } from "react";
-import { useSearchParams } from "next/navigation";
-import Experience from "@/components/experience/experience";
+import React, { Suspense } from 'react';
+import Home from './home';
 
-
-export default function Home() {
-  const searchParams = useSearchParams();
-
-useEffect(() => {
-  const scrollTo = searchParams.get('scrollTo');
-  if (scrollTo) {
-    const section = document.getElementById(scrollTo);
-    if (section) {
-      setTimeout(() => {
-        section.scrollIntoView({ behavior: 'smooth' });
-        window.history.replaceState({}, '', window.location.pathname);
-      }, 100);
-    }
-  }
-}, [searchParams]);
-
+export default function Page() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-    <>
-      <PageMetaHeads title="Home | My Portfolio" />
-      <div>
-        <Hero/>
-        <Projects />
-        <Tools />
-        <Experience />
-        <GetInTouch />
-      </div>
-    </>
+      <Home />
     </Suspense>
   );
 }
